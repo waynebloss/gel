f764cdaf98ca9d8e64cd7fd390d70efe8c73bf8f
Wayne Bloss <waynebloss@gmail.com>
2012-03-18 19:34:09 -0400

    Reformatted test code.

--- win/gel.exe/test/buffer.js
+++ win/gel.exe/test/buffer.js
@@ -6,32 +6,31 @@ exports.exec = function() {
 		arr[3] = 3;
 		console.log('arr has ' + arr.length + ' items.');
 
-		function ArrayBuffer(length) {
-
-			// The basica idea here is to pre-fill this array
-			// and tack our custom methods onto this instance
-			// to avoid modifying Array's prototype.
-			this.init(0, length);
-
-			this.custFn = function() { 
-				return 'ArrayBuffer has ' + this.length + ' items.';
-			};
-		}
-		ArrayBuffer.prototype = Array.prototype;
-
 		testBuffer(ArrayBuffer, 3);
-
-		function Buffer(length) {
-			// Prefilling this "array" here.
-			this.length = length;
-			while (length--) { this[length] = 0; }
-			//length = this.length;
-		}
-		Buffer.prototype.custFn = function() {
-			return 'Buffer has ' + this.length + ' items.';
-		};
-
 		testBuffer(Buffer, 3);
+};
+
+function ArrayBuffer(length) {
+
+	// The basica idea here is to pre-fill this array
+	// and tack our custom methods onto this instance
+	// to avoid modifying Array's prototype.
+	this.init(0, length);
+
+	this.custFn = function() { 
+		return 'ArrayBuffer has ' + this.length + ' items.';
+	};
+}
+ArrayBuffer.prototype = Array.prototype;
+
+function Buffer(length) {
+	// Prefilling this "array" here.
+	this.length = length;
+	while (length--) { this[length] = 0; }
+	//length = this.length;
+}
+Buffer.prototype.custFn = function() {
+	return 'Buffer has ' + this.length + ' items.';
 };
 
 function testBuffer(BufferClass, len) {
